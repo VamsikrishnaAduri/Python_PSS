955ba016e8d36580aede5c03dddf7cab4d36a008
VamsikrishnaAduri <62156016+VamsikrishnaAduri@users.noreply.github.com>
2021-08-08 00:08:34 +0530

    Update Smaller_and_Greater.py

--- Smaller_and_Greater.py
+++ Smaller_and_Greater.py
@@ -1,13 +1,14 @@
 def solve(A):
-  sorted_lst = sorted(A)
-  Strictly_Smaller_el = sorted_lst[0]
-  Srtictly_Larger_el = sorted_lst[-1]
-  Dummy = 0
-  for i in range(len(A)):
-    if A[i]>Strictly_Smaller_el:
-      if A[i]<Srtictly_Larger_el:
-        Dummy +=1
-  return Dummy
+        sorted_lst = sorted(A)
+        Strictly_Smaller_el = sorted_lst[0]
+        Srtictly_Larger_el = sorted_lst[-1]
+        Dummy = 0
+        for i in range(len(A)):
+                if A[i]>Strictly_Smaller_el:
+                        if A[i]<Srtictly_Larger_el:
+                                Dummy +=1
+                
+        return (Dummy)
  
   A = [1, 2, 3,78,98,0,34,56,12,89,90]
   solve(A)
